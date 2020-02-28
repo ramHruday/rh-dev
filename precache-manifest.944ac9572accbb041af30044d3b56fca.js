e0caa332360d0e71f9b5eee460a40b6fc85268c4
rhruday <ramahrudayb@gmail.com>
2020-02-29 00:12:14 +0530

    Updates

---
+++ precache-manifest.944ac9572accbb041af30044d3b56fca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d357fe13b51bb9b5e4390d822d522cf",
+    "url": "/rh-dev/index.html"
+  },
+  {
+    "revision": "b3ee52d853a33dbcba82",
+    "url": "/rh-dev/static/css/main.780fabe2.chunk.css"
+  },
+  {
+    "revision": "a3ac6e7c0777beff671c",
+    "url": "/rh-dev/static/js/2.a66e5e50.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/rh-dev/static/js/2.a66e5e50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3ee52d853a33dbcba82",
+    "url": "/rh-dev/static/js/main.f4d137a3.chunk.js"
+  },
+  {
+    "revision": "6ca343084d43028a691d",
+    "url": "/rh-dev/static/js/runtime-main.610b8f51.js"
+  },
+  {
+    "revision": "0e5f9fd7dd433655a944aef2d4f560f9",
+    "url": "/rh-dev/static/media/Vector.0e5f9fd7.svg"
+  },
+  {
+    "revision": "f6586620e99f171c4ec894c79da807ed",
+    "url": "/rh-dev/static/media/img-placeholder.f6586620.svg"
+  }
+]);
