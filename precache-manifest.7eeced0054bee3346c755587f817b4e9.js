b1915b94403caba050645133bd32c5d32e10f1e7
rhruday <ramahrudayb@gmail.com>
2020-03-11 19:34:59 +0530

    Updates

--- precache-manifest.7eeced0054bee3346c755587f817b4e9.js
+++ precache-manifest.7eeced0054bee3346c755587f817b4e9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0e1c4f351634452fc66298a5d38359e",
+    "revision": "e5226bc14d34b172b9f6beff56da1853",
     "url": "/rh-dev/index.html"
   },
   {
-    "revision": "3e0b68213dd43aa53cca",
+    "revision": "1ca6bf371fc37d5dcf31",
     "url": "/rh-dev/static/css/main.780fabe2.chunk.css"
   },
   {
-    "revision": "d8daade5fe57f83be374",
-    "url": "/rh-dev/static/js/2.a49ff5b1.chunk.js"
+    "revision": "2e7b88fd356112786c26",
+    "url": "/rh-dev/static/js/2.189e54fd.chunk.js"
   },
   {
     "revision": "86afaa925e0f432774bf95b19b6933fb",
-    "url": "/rh-dev/static/js/2.a49ff5b1.chunk.js.LICENSE"
+    "url": "/rh-dev/static/js/2.189e54fd.chunk.js.LICENSE"
   },
   {
-    "revision": "3e0b68213dd43aa53cca",
-    "url": "/rh-dev/static/js/main.0383df52.chunk.js"
+    "revision": "1ca6bf371fc37d5dcf31",
+    "url": "/rh-dev/static/js/main.14db5906.chunk.js"
   },
   {
     "revision": "6ca343084d43028a691d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rh-dev/precache-manifest.54ff03980eb9ab03face7b35425093f2.js"
+  "/rh-dev/precache-manifest.7eeced0054bee3346c755587f817b4e9.js"
 );
 
 self.addEventListener('message', (event) => {
